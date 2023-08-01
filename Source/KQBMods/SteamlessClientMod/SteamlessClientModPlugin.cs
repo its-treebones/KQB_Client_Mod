bc2de6449b45c74e52d1a9066c6c04f837c02ffa
billy <billy@DudsHomunculus>
2023-08-01 16:46:06 -0700

    add steamless client

---
+++ Source/KQBMods/SteamlessClientMod/SteamlessClientModPlugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SteamlessClientMod
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class SteamlessClientModPlugin : BaseUnityPlugin
+    {
+        private const string myGUID = "com.treebones.steamlessclientmod";
+        private const string pluginName = "Steamless Client Mod";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
