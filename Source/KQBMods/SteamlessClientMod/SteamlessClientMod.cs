83a1ef685032903a0d3c7c46e55e91651d61eacf
billy <billy@DudsHomunculus>
2023-08-03 14:09:26 -0700

    added steamless client

--- Source/KQBMods/SteamlessClientMod/SteamlessClientMod.cs
+++ Source/KQBMods/SteamlessClientMod/SteamlessClientMod.cs
@@ -9,55 +9,21 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using Newtonsoft.Json;
+using GameSparks.Core;
+using System;
+using GameLogic;
 
 namespace SteamlessClientMod
 {
-    [HarmonyPatch(typeof(GameManager))]
-    [HarmonyPatch("InitPlayerPrefs")]
-    public static class PlayerPrefs_Patch
+    [HarmonyPatch(typeof(MockClient))]
+    [HarmonyPatch("ValidateOwnership")]
+    public static class Valid_Patch
     {
-        public static void Prefix()
+        public static bool Prefix(MockClient __instance)
         {
-            LiquidBit.KillerQueenX.TestClient client = new LiquidBit.KillerQueenX.TestClient();
-            string path;
-            if (File.Exists("TestData.json"))
-            {
-                path = "TestData.json";
-            }
-            else
-            {
-                path = "TestData.json.sample";
-            }
-            TestData testData = JsonConvert.DeserializeObject<TestData>(File.ReadAllText(path));
-            GameManager.GMInstance.cvars.testAccountName = testData.accounts[0].username;
-            client.Init();
-            GameManager.GMInstance.platformClient = client;
+            __instance.GetType().GetField("steamInitialized", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(__instance, true);
+            return true;
         }
-    }
 
-    [HarmonyPatch(typeof(TestClient))]
-    [HarmonyPatch("DoLoginFlow")]
-    public static class Login_Patch
-    {
-        public static void Postfix(TestClient __instance)
-        {
-            string path;
-            if (File.Exists("TestData.json"))
-            {
-                path = "TestData.json";
-            }
-            else
-            {
-                path = "TestData.json.sample";
-            }
-            TestData.TestAccount testAccount = JsonConvert.DeserializeObject<TestData>(File.ReadAllText(path)).accounts[0];
-
-            GameLogic.Profile profile = new GameLogic.Profile();
-            profile.displayName = testAccount.displayName;
-            profile.avatarUrl = testAccount.avatarUrl;
-            profile.playerId = "00000-" + testAccount.displayName;
-            __instance.AssignProfile(profile);
-            __instance.SetAccount(testAccount);
-        }
     }
 }
